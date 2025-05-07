87ff7f5b33725aeb087ef6bbf86fb3d9348c43c0
player127-monster <sachindannagaraja@gmail.com>
2025-05-07 08:27:03 +0530

    optimize

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -21,8 +21,31 @@ import grad3 from '../assets/Services/grad3.avif';
 import grad4 from '../assets/Services/grad4.avif';
 
 
-  import ServiceCarousel from './ServiceCarousel';
   
+function ServiceCarousel({ title, images, height, imageWidth }) {
+  return (
+    <div className="group bg-white w-full relative overflow-hidden rounded-xl">
+      <div className="flex w-[200%] animate-scroll-horizontal">
+        {[...images, ...images].map((img, idx) => (
+          <img
+            key={idx}
+            src={img}
+            loading="lazy"
+            className={`${imageWidth} object-cover`}
+            style={{ height: `${height}px` }}
+            alt={`${title} image ${idx + 1}`}
+          />
+        ))}
+      </div>
+      <div className="absolute bottom-4 left-4 z-10">
+        <p className="text-4xl text-white md:text-textColor font-bold lg:group-hover:text-white lg:group-hover:text-5xl transition-all duration-700">
+          {title}
+        </p>
+      </div>
+    </div>
+  );
+}
+
 
   
 
@@ -43,24 +66,24 @@ import grad4 from '../assets/Services/grad4.avif';
             <ServiceCarousel title="Model Shoot" images={[model1, model2, model3]} height={400} imageWidth="w-1/6" />
           </div>
   
-          <div className="md:col-span-4 row-span-2 pt-5" id="wedding-shoot">
-            <ServiceCarousel title="Wedding Shoot" images={[wed1, wed2, wed3]} height={600} imageWidth="w-1/5" />
+          <div className="md:col-span-4 row-span-2" id="wedding-shoot">
+            <ServiceCarousel title="Wedding Shoot" images={[wed1, wed2, wed3]} height={550} imageWidth="w-1/5" />
           </div>
   
           <div className="md:col-span-4 row-span-2 pt-0" id="commercial-shoot">
-            <ServiceCarousel title="Portraits" images={[portrait1, portrait2]} height={700} imageWidth="w-1/2" />
+            <ServiceCarousel title="Portraits" images={[portrait1, portrait2]} height={500} imageWidth="w-1/2" />
           </div>
   
           <div className="col-span-4 row-span-2" id="graduation-shoot">
-            <ServiceCarousel title="Graduation Shoot" images={[grad1, grad2, grad3, grad4]} height={700} imageWidth="w-1/5" />
+            <ServiceCarousel title="Graduation Shoot" images={[grad1, grad2, grad3, grad4]} height={500} imageWidth="w-1/5" />
           </div>
   
           <div className="col-span-4 row-span-2" id="pre-wedding-shoot">
-            <ServiceCarousel title="Pre-Wedding Shoot" images={[prewed1, prewed2, prewed3]} height={800} imageWidth="w-1/2" />
+            <ServiceCarousel title="Pre-Wedding Shoot" images={[prewed1, prewed2, prewed3]} height={500} imageWidth="w-1/2" />
           </div>
   
           <div className="col-span-4 row-span-2" id="product-shoot">
-            <ServiceCarousel title="Product Shoot" images={[product1, product2, product3]} height={700} imageWidth="w-1/3" />
+            <ServiceCarousel title="Product Shoot" images={[product1, product2, product3]} height={565} imageWidth="w-1/3" />
           </div>
   
           <div className="col-span-4">
